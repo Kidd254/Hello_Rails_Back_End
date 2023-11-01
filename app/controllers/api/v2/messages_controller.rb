cb15bad3f69208958190039e33c3d93aca75ac3a
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-11-01 14:58:24 +0300

    Add a method in the messages Controller

--- app/controllers/api/v2/messages_controller.rb
+++ app/controllers/api/v2/messages_controller.rb
@@ -1,2 +1,10 @@
 class Api::V2::MessagesController < ApplicationController
+    def random_greeting
+        greeting = Greeting.order('RANDOM()').first
+        if greeting.nil?
+          render json: { error: 'Record not found' }, status: 500
+        else
+          render json: { message: greeting.text }
+        end
+    end
 end
